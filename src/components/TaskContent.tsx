65c38d4ecaa518531ed9d4865e5237e22eea0924
Lucas Ballonje <lucas.ballonje@ativo.com>
2024-05-28 17:21:38 -0300

    feat toDo Complete

--- src/components/TaskContent.tsx
+++ src/components/TaskContent.tsx
@@ -2,15 +2,30 @@ import styles from "./TaskContent.module.css"
 import { Trash } from "phosphor-react"
 
 
-export function TaskContent () {
+interface Task {
+    id: number;
+    text: string;
+    isCompleted: boolean;    
+}
+
+interface TaskContentProps {
+    task: Task;    
+    onToggleComplete: (id: number) => void;
+    onDeleteTask: (id: number) => void
+}
+  
+
+export function TaskContent ({ task, onToggleComplete, onDeleteTask }: TaskContentProps) {
     return (
         <div className={styles.task}>
             <label className={styles.checkBoxContainer}>
-                <input type="checkbox" className={styles.checkTask}/>
+                <input type="checkbox" className={styles.checkTask} checked={task.isCompleted} onChange={() => onToggleComplete(task.id)}/>
                 <span className={styles.checkMark}></span>
-                <p className={styles.text}>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                <p className={styles.text}>
+                    {task.text}
+                </p>
             </label>
-            <button title="Deletar comentarios" className={styles.deleteComment}>
+            <button title="Deletar comentarios" className={styles.deleteComment} onClick={() => onDeleteTask(task.id)}>
                 <Trash size={18} className={styles.icon}/>
             </button>
         </div>
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -3,18 +3,52 @@ import styles from "./Task.module.css"
 import { TaskContent } from "./TaskContent"
 import { FormEvent, useState } from "react"
 
+
+interface Task {
+    id: number;
+    text: string;
+    isCompleted: boolean;
+}
+
 export function Task() {
 
-    const [tasksList, setTasksLists] = useState([1, 2])
+    const [tasksList, setTasksLists] =  useState<Task[]>([]);
 
     function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
         event?.preventDefault()  
         
-        const newTaskText = event.currentTarget.description.value                
+        const newTaskText = event.currentTarget.description.value.trim() 
+
+        if(newTaskText == ""){
+            return;
+        }
         
-        setTasksLists([...tasksList, newTaskText])                
+        const newTask = {
+            id: tasksList.length + 1,
+            text: newTaskText,
+            isCompleted: false
+        }
         
+        setTasksLists([...tasksList, newTask ])                
+        
+        event.currentTarget.reset()
     }
+
+    function handleToggleTaskCompletion(id: number){
+        setTasksLists(prevTasks =>
+            prevTasks.map(task =>
+              task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+            )
+        );
+    }
+
+    function deleteTask(taskToDeleteId: number) {
+        const tasksWithoutDeletedOne = tasksList.filter(task => task.id !== taskToDeleteId);
+        setTasksLists(tasksWithoutDeletedOne);
+    }
+
+    const completedTasksCount = tasksList.filter(task => task.isCompleted).length;
+    
 
     return (
         <div>
@@ -31,13 +65,13 @@ export function Task() {
             </div>
             <div className={styles.tasks} >
                 <div className={styles.headerTaskContent}>
-                    <span className={styles.createdTask}>Tarefas criadas <span className={styles.count}>5</span> </span>
+                    <span className={styles.createdTask}>Tarefas criadas <span className={styles.count}>{tasksList.length}</span> </span>
 
-                    <span className={styles.completeTask}>Concluidas <span className={styles.completeCount}>2 de 5</span></span>
+                    <span className={styles.completeTask}>Concluidas <span className={styles.completeCount}>{completedTasksCount} de {tasksList.length}</span></span>
                 </div>
                 <div className={styles.listContentTask}>
-                    {tasksList.map(taskList => {
-                        return <TaskContent />
+                    {tasksList.map((taskList) => {
+                        return <TaskContent key={taskList.id} task={taskList} onToggleComplete={handleToggleTaskCompletion} onDeleteTask={deleteTask}/>
                     })}
                 </div>            
             </div>
